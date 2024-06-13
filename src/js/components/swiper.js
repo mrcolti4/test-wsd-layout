762751b19424a9f5cc21fb88ec3e763fd33c4f99
mrcolti4 <kbulygin456@gmail.com>
2024-06-13 20:05:23 +0300

    feat: add images on page and fix styles

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -3,21 +3,18 @@ import { Navigation, Pagination } from "swiper/modules";
 
 const swiper = new Swiper(".swiper", {
   modules: [Navigation, Pagination],
-  // loop: true,
 
-  // // If we need pagination
+  // Fixed with for slides
+  slidesPerView: "auto",
+
+  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
+    clickable: true
   },
-
-  // // Navigation arrows
+  // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next-unique",
+    prevEl: ".swiper-button-prev-unique",
   },
-
-  // // And if we need scrollbar
-  // scrollbar: {
-  //   el: ".swiper-scrollbar",
-  // },
 });
