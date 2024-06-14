52927909c5a369032e8234bfe7b026d4b20e3b3a
mrcolti4 <kbulygin456@gmail.com>
2024-06-14 09:36:28 +0300

    feat: create mobile menu and fix hero styles

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,4 +1,14 @@
+// Script for sticky header
 window.addEventListener("scroll", () => {
     const header = document.querySelector("header.header");
     header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+// Script for mobile menu
+const menuIcon = document.querySelector(".header__icon")
+const menuNav = document.querySelector("nav.header__navigation");
+menuIcon.addEventListener("click", (e) => {
+    document.body.classList.toggle("lock")
+    menuNav.classList.toggle("open");
+    menuIcon.classList.toggle("_active")
 })
