6f4c542f8b123136c27e4ba5c921608056428b4d
mrcolti4 <kbulygin456@gmail.com>
2024-06-14 08:24:05 +0300

    feat: add media queries for last sections

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -5,16 +5,22 @@ const swiper = new Swiper(".swiper", {
   modules: [Navigation, Pagination],
 
   // Fixed with for slides
-  slidesPerView: "auto",
+  slidesPerView: "1",
 
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
-    clickable: true
+    clickable: true,
   },
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next-unique",
     prevEl: ".swiper-button-prev-unique",
+  },
+
+  autoplay: {
+    delay: 5000
+  },
+  breakpoints: {
   },
 });
