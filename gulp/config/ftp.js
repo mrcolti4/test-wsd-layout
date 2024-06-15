daadc92f2754486dcca8556f7a436c5d8cd160d4
Kyrylo Bulyhin <kyrylocolti4@gmail.com>
2024-06-15 06:34:14 +0300

    init wordpress

--- gulp/config/ftp.js
+++
@@ -1,6 +0,0 @@
-export let configFTP = {
-    host: "", // FTP-server address
-    user: "", // Username
-    password: "", // Password
-    parallel: 5 // number of concurrent streams
-}
--- src/js/app.js
+++
@@ -1,6 +0,0 @@
-import * as flsFunctions from "./modules/functions.js";
-import "./components/swiper.js";
-import "./components/header.js";
-import "./components/validate.js"
-
-flsFunctions.isWebp();
